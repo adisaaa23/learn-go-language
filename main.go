e5f7e8bf81f0284fadf28b5bff9abc4665a2a73e
Adi Saputra <adisaputra@Adis-MacBook-Pro.local>
2022-04-29 23:42:42 +0700

    add endpoint /articles with return dummy data

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,11 +12,29 @@ func mainPage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Call Endpoint / ")
 }
 
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Call Endpoint /articles")
+	json.NewEncoder(w).Encode(Articles)
+}
+
 func handleReq() {
 	http.HandleFunc("/", mainPage)
+	http.HandleFunc("/articles", allArticles)
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
 func main() {
+	Articles = []Article{
+		Article{Title: "John Doe", Desc: "John Doe Descriptions ...", Content: "John Doe Contents..."},
+		Article{Title: "John Doe 1", Desc: "John Doe Descriptions 2 ...", Content: "John Doe Contents 2 ..."},
+	}
 	handleReq()
 }
+
+type Article struct {
+	Title   string `json:"title"`
+	Desc    string `json:"desc"`
+	Content string `json:content`
+}
+
+var Articles []Article
